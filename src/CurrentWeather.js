31b8259c3f12f2f8bafd013b2a7366a5eaab27f0
uehara-yuki <78370313+uehara-yuki@users.noreply.github.com>
2021-05-17 12:42:59 +0200

    trying to install axios-api integration

--- src/CurrentWeather.js
+++ src/CurrentWeather.js
@@ -1,8 +1,23 @@
-import React from "react";
-
+import React, { useState } from "react";
+import axios from "axios";
 import "./CurrentWeather.css";
 
+
+
 export default function CurrentWeather() {
+  const [weatherData, setWeatherData]= useState ({ready: false});
+  
+  function handleResponse(response){
+    console.log(response.data)
+    setWeatherData ({
+      ready:true,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+
+    });
+  }
+if (weatherData.ready) {
   return (
     <div className="row">
       <div className="col-6">
@@ -21,10 +36,10 @@ export default function CurrentWeather() {
       <div className="col-6">
         <div className="weatherDescription" id="weather-description">
           {" "}
-          Clear Sky{" "}
+          {weatherData.description}Clear Sky{" "}
         </div>
         <div className="humidity" id="humidity">
-          Humidity: 5%
+          {weatherData.humidity}Humidity: 5%
         </div>
         <div className="wind" id="wind">
           {" "}
@@ -33,4 +48,15 @@ export default function CurrentWeather() {
       </div>
     </div>
   );
+} else {
+  
+const apiKey="c25c2e288aa866c69cd6db4b9732a68a";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+let city= "New York";
+axios.get(apiUrl).then(handleResponse);
+}
+return (
+  "Loading..."
+);
+  
 }
