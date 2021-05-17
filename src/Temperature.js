8311668aa1c3f35822c2beb36f5b9a6eb24dc788
uehara-yuki <78370313+uehara-yuki@users.noreply.github.com>
2021-05-17 18:55:41 +0200

    imported axios and created function temperature

--- src/Temperature.js
+++ src/Temperature.js
@@ -1,12 +1,23 @@
-import React from "react";
-
+import React, {useState} from "react";
+import axios from "axios";
 import "./Temperature.css";
 
 export default function Temperature() {
-  return (
+   const [temperature, setTemperature] = useState ({ready: false});
+
+   function handleResponse(response){
+     console.log(response.data);
+     setTemperature({
+       ready: true,
+       temperature: response.data.main.temp,
+     })
+   }
+
+  if (temperature.ready) {
+    return (
     <div className="temperature">
       <div>
-        <span id="temperature"> 19 </span>
+        <span id="temperature"> {Math.round(temperature)}</span>
         <a href="/" id="celsius" className="active">
           °C
         </a>
@@ -17,4 +28,17 @@ export default function Temperature() {
       </div>
     </div>
   );
+
+
+  } else {
+      
+const apiKey="c25c2e288aa866c69cd6db4b9732a68a";
+let city = "New York";
+let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(handleResponse);
 }
+return (
+  "Loading..."
+);
+  }
